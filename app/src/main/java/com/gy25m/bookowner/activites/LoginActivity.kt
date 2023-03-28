80cb525f25f64e61c441021b5ba18c8ddc2e4f24
kimgyeom2 <kimgyeom2@gmail.com>
2023-03-28 14:28:01 +0900

    Login,JoinMembership actitivty

--- app/src/main/java/com/gy25m/bookowner/activites/LoginActivity.kt
+++ app/src/main/java/com/gy25m/bookowner/activites/LoginActivity.kt
@@ -1,12 +1,20 @@
 package com.gy25m.bookowner.activites
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.gy25m.bookowner.R
+import com.gy25m.bookowner.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val binding=ActivityLoginBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+        binding.ivBefore.setOnClickListener{finish()}
+        binding.nomem.setOnClickListener{
+            intent= Intent(this,MembershipActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/gy25m/bookowner/activites/MembershipActivity.kt
+++ app/src/main/java/com/gy25m/bookowner/activites/MembershipActivity.kt
@@ -3,10 +3,13 @@ package com.gy25m.bookowner.activites
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.gy25m.bookowner.R
+import com.gy25m.bookowner.databinding.ActivityMembershipBinding
 
 class MembershipActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val binding=ActivityMembershipBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_membership)
+        setContentView(binding.root)
+        binding.ivBefore.setOnClickListener { finish() }
     }
 }
