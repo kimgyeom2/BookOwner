b23c6e172b594503235675bda9aa257fc447f649
kimgyeom2 <kimgyeom2@gmail.com>
2023-04-03 17:49:39 +0900

    4/3 Login수정해야됨 searchrecycleritem -ing

--- app/src/main/java/com/gy25m/bookowner/model/AladinApiResponce.kt
+++ app/src/main/java/com/gy25m/bookowner/model/AladinApiResponce.kt
@@ -1,3 +1,3 @@
 package com.gy25m.bookowner.model
 
-data class AladinApiResponce(var title:String,var author:String,var pubDate:String,var description:String)
+data class AladinApiResponce(var title:String,var author:String,var pubDate:String,var cover:String,var description:String)
