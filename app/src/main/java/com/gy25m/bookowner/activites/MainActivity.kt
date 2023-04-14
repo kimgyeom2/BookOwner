8741801b4c8a1a5439c49cfee22ab59ce660acc0
kimgyeom2 <kimgyeom2@gmail.com>
2023-04-14 10:29:31 +0900

    map 마커 수정2

--- app/src/main/java/com/gy25m/bookowner/activites/MainActivity.kt
+++ app/src/main/java/com/gy25m/bookowner/activites/MainActivity.kt
@@ -38,10 +38,7 @@ import retrofit2.Retrofit
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    var searchQuery:String="서점"
-    var myLocation: Location?=null
-    val providerClient: FusedLocationProviderClient by lazy { LocationServices.getFusedLocationProviderClient(this) }
-    var searchPlaceResponse: KakaoSearchPlaceResponse?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -54,77 +51,9 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this,MapActivity::class.java))
         }
 
-        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_DENIED){
-            // 퍼미션 요청 대행사 이용-계약 체결
-            permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-        }else{
-            // 내 위치 요청
-            requestMyLocation()
-        }
-    }
-    val permissionLauncher: ActivityResultLauncher<String> =registerForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ) { result ->
-        if (result!!) requestMyLocation()
-        else Toast.makeText(
-            this@MainActivity,
-            "위치정보 제공에 동의하지 않았습니다. 검색기능이 제한됩니다",
-            Toast.LENGTH_SHORT
-        ).show()
-    }
-
-    private fun requestMyLocation(){
-        // 위치검색 기준 설정하는 요청객체
-        val request: LocationRequest =
-            LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY,1000).build()
-
-        // 실시간 위치정보를 갱신하도록 요청
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            return
-        }   // if문 error클릭 자동완성
-        providerClient.requestLocationUpdates(request,locationCallback, Looper.getMainLooper())
-    }
-
-
-    private val locationCallback: LocationCallback =object : LocationCallback(){
-        override fun onLocationResult(p0: LocationResult) {
-            super.onLocationResult(p0)
-            myLocation=p0.lastLocation
-
-            //위치 탐색되었으니 실시간 업데이트를 종료
-            providerClient.removeLocationUpdates(this) //this : locationCallback 객체
-
-            searchPlace()
-        }
-    }
-    private fun searchPlace(){
-        val retrofit: Retrofit = RetrofitHelper.getRetrofitInstance("https://dapi.kakao.com")
-        val retrofitApiservice=retrofit.create(RetrofitApiService::class.java)
-        retrofitApiservice.searchplace(searchQuery,myLocation?.latitude.toString(),myLocation?.longitude.toString()).enqueue(object :
-            Callback<KakaoSearchPlaceResponse> {
-            override fun onResponse(
-                call: Call<KakaoSearchPlaceResponse>,
-                response: Response<KakaoSearchPlaceResponse>
-            ) {
-                searchPlaceResponse=response.body()
-
-                //Toast.makeText(this@MainActivity, "${searchPlaceResponse?.meta?.total_count}", Toast.LENGTH_SHORT).show()
-
-            }
-
-            override fun onFailure(call: Call<KakaoSearchPlaceResponse>, t: Throwable) {
-                Toast.makeText(this@MainActivity, "서버문제가 있습니다", Toast.LENGTH_SHORT).show()
-            }
-        })
 
     }
+
 
     private fun bnvControl(){
         binding.bnv.run {
